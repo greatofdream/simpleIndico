e93b18e7705d1272cd198469cfb8c7ba099108df
greatofdream <greatofdream@gmail.com>
2021-11-21 15:16:35 +0800

    fix the navbar style using flex

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="homepage">
-    <el-container style="height: 1000px;">
-      <el-header>
+    <el-container>
+      <el-header height="auto">
         <el-menu
           :default-active="activeHeadIndex"
           class="head-menu"
@@ -9,16 +9,17 @@
           background-color="#78C2AD"
           text-color="#fff"
           active-text-color="#000">
-          <el-menu-item index="1"><router-link to='/meet/list' class="navbutton">Meeting Info</router-link></el-menu-item>
-          <el-menu-item index="2"><a href="http://hep.tsinghua.edu.cn/about.html" class="navbutton">About TUHEP</a></el-menu-item>
-          <el-menu-item index="3"><a href="http://hep.tsinghua.edu.cn/committee.html" class="navbutton">Committee</a></el-menu-item>
-          <el-menu-item index="4"><a href="http://hep.tsinghua.edu.cn/committee.html" class="navbutton">Member</a></el-menu-item>
-          <el-menu-item index="5"><a href="http://hep.tsinghua.edu.cn/research.html" class="navbutton">Research</a></el-menu-item>
-          <el-menu-item index="6"><a href="http://hep.tsinghua.edu.cn/empolyment.html" class="navbutton">Employment</a></el-menu-item>
-          <el-menu-item index="7"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">Publication</a></el-menu-item>
-          <el-menu-item index="8"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">Thesis</a></el-menu-item>
-          <el-menu-item index="9"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">File Download</a></el-menu-item>
-          <el-menu-item index="10"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">Training</a></el-menu-item>
+          <el-menu-item class="item" index="1"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">主页<br/>Home</a></el-menu-item>
+          <el-menu-item class="item" index="2"><a href="http://hep.tsinghua.edu.cn/about.html" class="navbutton">中心简介<br/>About</a></el-menu-item>
+          <el-menu-item class="item" index="3"><a href="http://hep.tsinghua.edu.cn/committee.html" class="navbutton">学术委员会<br/>Committee</a></el-menu-item>
+          <el-menu-item class="item" index="4"><a href="http://hep.tsinghua.edu.cn/committee.html" class="navbutton">中心成员<br/>Member</a></el-menu-item>
+          <el-menu-item class="item" index="5"><a href="http://hep.tsinghua.edu.cn/research.html" class="navbutton">研究方向<br/>Research</a></el-menu-item>
+          <el-menu-item class="item" index="6"><a href="http://hep.tsinghua.edu.cn/empolyment.html" class="navbutton">招聘信息<br/>Employment</a></el-menu-item>
+          <el-menu-item class="item" index="7"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">发表<br/>Publication</a></el-menu-item>
+          <el-menu-item class="item" index="8"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">论文<br/>Thesis</a></el-menu-item>
+          <el-menu-item class="item" index="9"><a href="http://hep.tsinghua.edu.cn/" class="navbutton">文件下载<br/>File Download</a></el-menu-item>
+          <el-menu-item class="item" index="10"><a href="http://hep.tsinghua.edu.cn/training.html" class="navbutton">学术训练<br/>Training</a></el-menu-item>
+          <el-menu-item class="item" index="11"><router-link to='/meet/list' class="navbutton">会议记录<br/>Meeting Info</router-link></el-menu-item>
         </el-menu>
       </el-header>
       <el-container class="containerbody">
@@ -33,7 +34,19 @@ export default {
   name: 'HomePage',
   data: function() {
     return {
-      activeHeadIndex: "1",
+      activeHeadIndex: "11",
+      navinfo: [
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+        {href:'hep.tsinghua.edu.cn', text: '主页/brHome'},
+      ]
     }
   },
   methods: {
@@ -51,5 +64,20 @@ export default {
   font-size: 1rem;
   font-weight: 400;
   line-height: 1.5;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  padding-left: 0;
+  padding-right: 0;
+  display: block;
+}
+.item{
+  flex-basis: 0;
+  flex-grow: 1;
+}
+.head-menu{
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0.5rem 1rem;
 }
 </style>
